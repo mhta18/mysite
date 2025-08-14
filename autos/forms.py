3a1ee6160de38da1cc238d9d7e0ce82ba9cc3214
mahta mira <mahta.m.1183@gmail.com>
2025-08-14 15:40:45 +0330

    Initial commit

---
+++ autos/forms.py
@@ -0,0 +1,10 @@
+# autos/forms.py
+from django.forms import ModelForm
+from autos.models import Make
+
+
+# Create the form class.
+class MakeForm(ModelForm):
+    class Meta:
+        model = Make
+        fields = '__all__'
